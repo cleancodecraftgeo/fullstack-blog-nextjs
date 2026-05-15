d0bd8c40023abdce6b68e7098abe1d26bea54cef
elnur549 <elnurtaken@gmail.com>
2026-05-16 02:12:57 +0400

    feat: add Prisma ORM, MySQL database connection, real data fetching via API routes

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -1,10 +1,6 @@
-const posts = [
-  { id: 1, title: "Next.js ilə ilk addım", content: "Bu mənim ilk postdur!" },
-  { id: 2, title: "Tailwind CSS haqqında", content: "Tailwind çox rahatdır." },
-  { id: 3, title: "React vs Next.js", content: "Next.js daha güclüdür." },
-  { id: 4, title: "React vs Vue.js", content: "Nuxt.js daha güclüdür." },
-]
+import {prisma} from "@/lib/db"
 
 export async function GET() {
+  const posts = await prisma.post.findMany({ orderBy:{createdAt:"desc"}})
   return Response.json(posts)
 }
